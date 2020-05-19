9d750c736650a8f6774d62f85c61623b8b2e51c2
Jason M Grayson <jaymgrayson@gmail.com>
2020-05-19 07:35:51 -0400

    Update Figure 1B_final.R

--- Figure 1B_final.R
+++ Figure 1B_final.R
@@ -14,25 +14,9 @@ setwd("~/Your working directory")
 load("~Figure1a_subsampled.Rda")
 Heat_Map_All<-Subsampled
 
-# Color by protein values for each marker
-CD3<-ggplot(Heat_Map_All, aes(x=V1, y=V2, color=CD3)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD3 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD3")
-CD3
-CD3+scale_color_gradient(low="blue", high="red")
+# Color by protein values for each marker, LAG-3 is shown as an example
 
-CD8<-ggplot(Heat_Map_All, aes(x=V1, y=V2, color=CD8)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD8 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD8")
-CD8
-CD8+scale_color_gradient(low="blue", high="red")
-
-CD4<-ggplot(Heat_Map_All, aes(x=V1, y=V2, color=CD4)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD4 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD4")
-CD4
-CD4+scale_color_gradient(low="blue", high="red")
 
 LAG_3<-ggplot(Heat_Map_All, aes(x=V1, y=V2, color=`LAG-3`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of LAG-3 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "LAG-3")
 LAG_3
 LAG_3+scale_color_gradient(low="blue", high="red")
-
-PD_1<-ggplot(Heat_Map_All, aes(x=V1, y=V2, color=`PD-1`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of PD-1 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "PD-1")
-PD_1
-PD_1+scale_color_gradient(low="blue", high="red")
-
-MLR_PD<-Subsampled[Treatment=="MLR+PD",]
