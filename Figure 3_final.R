e2353b653d187435cb8a8307cf310e3d689df121
Jason M Grayson <jaymgrayson@gmail.com>
2020-05-19 07:37:48 -0400

    Update Figure 3_final.R

--- Figure 3_final.R
+++ Figure 3_final.R
@@ -35,50 +35,8 @@ g2
 
 #Code for supplemental figures
 # Color by protein values for each marker
-CD3<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=CD3)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD3 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD3")
-CD3
-CD3+scale_color_gradient(low="blue", high="red")
-
-CD8<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=CD8)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD8 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD8")
-CD8
-CD8+scale_color_gradient(low="blue", high="red")
-
-CD4<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=CD4)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD4 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD4")
-CD4
-CD4+scale_color_gradient(low="blue", high="red")
+# Example shown for LAG-3
 
 LAG_3<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`LAG-3`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of LAG-3 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "LAG-3")
 LAG_3
 LAG_3+scale_color_gradient(low="blue", high="red")
-
-PD_1<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`PD-1`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of PD-1 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "PD-1")
-PD_1
-PD_1+scale_color_gradient(low="blue", high="red")
-
-CD44<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`CD44`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD44 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD44")
-CD44
-CD44+scale_color_gradient(low="blue", high="red")
-
-CD62L<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`CD62L`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD62L in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD62L")
-CD62L
-CD62L+scale_color_gradient(low="blue", high="red")
-
-IFNg<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`IFNg`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of IFNg in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "IFNg")
-IFNg
-IFNg+scale_color_gradient(low="blue", high="red")
-
-TNFa<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`TNFa`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of TNFa in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "TNFa")
-TNFa
-TNFa+scale_color_gradient(low="blue", high="red")
-
-IL_2<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`IL-2`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of IL-2 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "IL-2")
-IL_2
-IL_2+scale_color_gradient(low="blue", high="red")
-
-CTLA_4<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`CTLA-4`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CTLA-4 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CTLA-4")
-CTLA_4
-CTLA_4+scale_color_gradient(low="blue", high="red")
-
-CD25a<-ggplot(Subsampled3a, aes(x=V1, y=V2, color=`CD25a`)) + geom_point(size=0.1)+facet_wrap(~Treatment)+theme_bw()+theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+ ggtitle("Heatmap of CD25 in Various Treatments of Mice") + theme(plot.title = element_text(hjust = 0.5))+ labs(colour = "CD25a")
-CD25a
-CD25a+scale_color_gradient(low="blue", high="red")
